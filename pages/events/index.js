c8f0981d36d492587bb6690472ea37b9378cad26
tojimato <tojimato@gmail.com>
2023-12-07 13:16:50 +0300

    final

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { Fragment } from "react";
+import {useRouter} from 'next/router';
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/event-search";
 
 function AllEventsPage() {
-  return <div>AllEventsPage</div>;
+  const router = useRouter();
+  const events = getAllEvents();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
+  return (
+    <Fragment>
+      <EventsSearch onSearch={findEventsHandler}></EventsSearch>
+      <EventList items={events} />
+    </Fragment>
+  );
 }
 
 export default AllEventsPage;
